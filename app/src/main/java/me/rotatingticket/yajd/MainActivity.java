541061345d9806c50bc120e0bab5c481d00ecc89
RTNelo <rtnelo@yeah.net>
2018-05-04 16:45:58 +0800

    feat(dict.impl.core.sqlite): add getInstance method to SQLiteDatabase

--- app/src/main/java/me/rotatingticket/yajd/MainActivity.java
+++ app/src/main/java/me/rotatingticket/yajd/MainActivity.java
@@ -1,44 +1,108 @@
 package me.rotatingticket.yajd;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.List;
+
+import me.rotatingticket.yajd.dict.core.WordEntry;
+import me.rotatingticket.yajd.view.CandidateView;
+import me.rotatingticket.yajd.viewmodel.MainActivityViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private CardView candidatesCardView;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private static class CandidatesAdapter extends BaseAdapter {
+
+        Context context;
+        private List<? extends WordEntry> candidates;
+
+        CandidatesAdapter(Context context, List<? extends WordEntry> candidates) {
+            this.context = context;
+            this.candidates = candidates;
+        }
+
+        public void setCandidates(List<? extends WordEntry> candidates) {
+            this.candidates = candidates;
+        }
 
         @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
+        public int getCount() {
+            return candidates.size();
         }
-    };
+
+        @Override
+        public WordEntry getItem(int position) {
+            return candidates.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(context).inflate(R.layout.search_candidate, parent, false);
+            }
+            ((CandidateView)convertView).setWordEntry(getItem(position));
+            return convertView;
+        }
+    }
+
+    private MainActivityViewModel viewModel;
+    private CandidatesAdapter candidatesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+        candidatesCardView = findViewById(R.id.candidates_card_view);
+        ListView candidatesView = findViewById(R.id.candidates_view);
+        setUpCandidatesView(candidatesView);
+        SearchView searchView = findViewById(R.id.search_view);
+        setUpSearchView(searchView);
     }
 
+    private void setUpCandidatesView(ListView candidatesView) {
+        viewModel.getCandidates().observe(this, candidates -> {
+            candidatesCardView.setVisibility(
+                  candidates == null || candidates.size() == 0 ? View.INVISIBLE : View.VISIBLE
+            );
+            if (candidatesAdapter == null) {
+                candidatesAdapter = new CandidatesAdapter(this, candidates);
+                candidatesView.setAdapter(candidatesAdapter);
+            } else {
+                candidatesAdapter.setCandidates(candidates);
+            }
+        });
+    }
+
+    private void setUpSearchView(SearchView searchView) {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                viewModel.query(newText);
+                return true;
+            }
+        });
+    }
 }
--- app/src/main/java/me/rotatingticket/yajd/dict/implementation/core/sqlite/SQLiteDictDatabase.java
+++ app/src/main/java/me/rotatingticket/yajd/dict/implementation/core/sqlite/SQLiteDictDatabase.java
@@ -1,12 +1,24 @@
 package me.rotatingticket.yajd.dict.implementation.core.sqlite;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 /**
  * The database of SQLiteDictCore.
  */
 @Database(version = 1, entities = {SQLiteWordRecord.class, SQLiteWordRomaji.class})
 public abstract class SQLiteDictDatabase extends RoomDatabase {
+    private static SQLiteDictDatabase instance;
+    private static final String DB_NAME = "DictCore.db";
+
     abstract public SQLiteDictCore getSQLiteCoreDict();
+
+    public static synchronized SQLiteDictDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context, SQLiteDictDatabase.class, DB_NAME).build();
+        }
+        return instance;
+    }
 }
